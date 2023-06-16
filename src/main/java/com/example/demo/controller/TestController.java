eb309e21375f8168576efe4eb607f29290358dc0
Lim Suyeon <shycat4151@gmail.com>
2023-06-16 19:57:22 +0900

    백엔드 개발 11 @RequestMapping, @GetMapping

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -20,5 +22,11 @@ public class TestController {
 	public String testControllerWithPathVariables(@PathVariable(required =false) int id){
 		return "Hello World! ID " + id;
 	}
+	
+	@GetMapping("/Param")
+	public String testContollerRequestParam(@RequestParam(required =false) int id) {
+		return "Hello World! ID param " + id;
+	}
+	
 
 }
